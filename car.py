cf1c3ccc62c519bdee4d220f59ab17c95aa8a870
ifeoluwa Adefioye <ifeoluwa@Seuns-MacBook-Air.local>
2023-11-02 14:09:59 +0100

    refactored car.py

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-
+from datetime import date
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date: date):
         self.last_service_date = last_service_date
 
     @abstractmethod
-    def needs_service(self):
+    def needs_service(self) -> bool:
         pass
+
